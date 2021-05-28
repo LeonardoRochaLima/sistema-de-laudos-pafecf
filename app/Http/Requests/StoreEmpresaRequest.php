5bdbff41db10e6a72dcfecac4644a63d0141b4c0
Your Name <leonardorocha.limaa@gmail.com>
2021-05-28 13:58:22 -0300

    Adicionando validação de campos no formulário de cadastro de empresas

--- app/Http/Requests/StoreEmpresaRequest.php
+++ app/Http/Requests/StoreEmpresaRequest.php
@@ -34,12 +34,29 @@ class StoreEmpresaRequest extends FormRequest
             'telefone' => 'required',
             'celular' => 'required',
             'cnpj' => 'required',
-            'inscricao_estadual' => 'required',
-            'inscricao_municipal' => 'required',
             'representante' => 'required',
             'cpf_representante' => 'required',
             'rg_representante' => 'required',
             'email_representante' => 'required',
         ];
+    }
+
+    public function messages(){
+        return[
+            'razao_social.required' => 'O campo razão social é obrigatório',
+            'nome_fantasia.required' => 'O campo nome fantasia é obrigatório',
+            'endereco.required' => 'O campo endereço é obrigatório',
+            'bairro.required' => 'O campo bairro é obrigatório',
+            'cidade.required' => 'O campo cidade é obrigatório',
+            'uf.required' => 'O campo UF é obrigatório',
+            'cep.required' => 'O campo CEP é obrigatório',
+            'telefone.required' => 'O campo telefone é obrigatório',
+            'celular.required' => 'O campo celular é obrigatório',
+            'cnpj.required' => 'O campo CNPJ é obrigatório',
+            'representante.required' => 'O campo Representante é obrigatório',
+            'cpf_representante.required' => 'O campo CPF é obrigatório',
+            'rg_representante.required' => 'O campo RG é obrigatório',
+            'email_representante.required' => 'O campo Email é obrigatório',
+        ]
     }
 }
--- resources/views/cadastros/create.blade.php
+++ resources/views/cadastros/create.blade.php
@@ -8,7 +8,7 @@
         <div class="alert alert-danger">
             <ul>
                 @foreach ($errors->all() as $error)
-                    <li>{{$error}}</li>
+                    <li>{{ $error }}</li>
                 @endforeach
             </ul>
         </div>
@@ -24,11 +24,11 @@
                 <form class="row">
                     <div class="form-group col-md-7">
                         <label for="razao_social">Razão Social</label>
-                        <input type="text" class="form-control @error('razao_social') is-invalid @enderror" id="razao_social" name="razao_social"
-                            placeholder="Razão Social">
+                        <input type="text" class="form-control @error('razao_social') is-invalid @enderror"
+                            id="razao_social" name="razao_social" placeholder="Razão Social">
                         @error('razao_social')
-                            <div class="invalid-feedback">
-                                {{$message}}
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
                             </div>
                         @enderror
                     </div>
@@ -36,38 +36,83 @@
                         <label for="nome_fantasia">Nome Fantasia</label>
                         <input type="text" class="form-control" id="nome_fantasia" name="nome_fantasia"
                             placeholder="Nome Fantasia">
+                        @error('nome_fantasia')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group col-md-7">
                         <label for="endereco">Endereço</label>
                         <input type="text" class="form-control" id="endereco" name="endereco" placeholder="Endereço">
+                        @error('endereco')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group col-md-5">
                         <label for="bairro">Bairro</label>
                         <input type="text" class="form-control" id="bairro" name="bairro" placeholder="Bairro">
+                        @error('bairro')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group col-md-6">
                         <label for="cidade">Cidade</label>
                         <input type="text" class="form-control" id="cidade" name="cidade" placeholder="Cidade">
+                        @error('cidade')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group col-md-2">
                         <label for="uf">UF</label>
                         <input type="text" class="form-control" id="uf" name="uf" placeholder="UF">
+                        @error('uf')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group col-md-4">
                         <label for="cep">CEP</label>
                         <input type="text" class="form-control" id="cep" name="cep" placeholder="CEP">
+                        @error('cep')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group col-md-6">
                         <label for="telefone">Telefone</label>
                         <input type="text" class="form-control" id="telefone" name="telefone" placeholder="Telefone">
+                        @error('telefone')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group col-md-6">
                         <label for="celular">Celular</label>
                         <input type="text" class="form-control" id="celular" name="celular" placeholder="Celular">
+                        @error('celular')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group col-md-4">
                         <label for="cnpj">CNPJ</label>
                         <input type="text" class="form-control" id="cnpj" name="cnpj" placeholder="CNPJ">
+                        @error('cnpj')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group col-md-4">
                         <label for="inscricao_estadual">Inscrição Estadual</label>
@@ -83,21 +128,41 @@
                         <label for="representante">Representante Legal da Empresa</label>
                         <input type="text" class="form-control" id="representante" name="representante"
                             placeholder="Representante Legal da Empresa">
+                        @error('representante')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group col-md-4">
                         <label for="cpf_representante">CPF do Responsável</label>
                         <input type="text" class="form-control" id="cpf_representante" name="cpf_representante"
                             placeholder="CPF do Representante">
+                        @error('cpf_representante')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group col-md-4">
                         <label for="rg_representante">RG do Representante</label>
                         <input type="text" class="form-control" id="rg_representante" name="rg_representante"
                             placeholder="RG do Representante">
+                        @error('rg_representante')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group col-md-12">
                         <label for="email_representante">Email de Contato - Representante</label>
                         <input type="text" class="form-control" id="email_representante" name="email_representante"
                             placeholder="Email">
+                        @error('email_representante')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group col-md-2">
                         <input type="submit" class="btn btn-success" value="Finalizar Cadastro">
