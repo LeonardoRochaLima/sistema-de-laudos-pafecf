13239bd225c05e738f6537ff33095f0899557c6e
Leonardo Lima <leonardo.lima@edu.unifil.br>
2021-07-07 16:25:20 -0300

    Atualizando os botões de exclusão e edição

--- resources/views/cadastros/show.blade.php
+++ resources/views/cadastros/show.blade.php
@@ -11,11 +11,10 @@
             margin-left: -110%;
             margin-top: -40%;
         }
-
     </style>
     <script>
         function validarExclusao(frm) {
-            var validador = confirm("Tem certeza que deseja excluir o cadastro desta em");
+            var validador = confirm("Tem certeza que deseja excluir o cadastro desta empresa?");
             if (validador == false) {
                 return false;
             } else {
@@ -28,7 +27,7 @@
         @csrf
         <button type="submit" class="btn btn-outline-danger">
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash-fill"
-                viewBox="0 0 16 16" style="color: red">
+                viewBox="0 0 16 13" style="color: red">
                 <path
                     d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z">
                 </path>
