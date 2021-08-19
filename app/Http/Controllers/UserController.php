4f3ff6172d6be4691ea3d9e72c299b3f4faee401
Leonardo Lima <leonardo.lima@edu.unifil.br>
2021-08-19 17:03:13 -0300

    Criando as views para visualização e edição de perfil de usuário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function show(){
+        return view('profile.show');
+    }
+
+    public function update(){
+
+    }
 }
