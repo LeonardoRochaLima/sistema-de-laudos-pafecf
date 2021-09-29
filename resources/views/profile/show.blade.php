45da1620cb27a12d930171c39726aa6d1412522a
Leonardo Lima <leonardo.lima@edu.unifil.br>
2021-09-29 13:19:09 -0300

    Dividindo o formulário de alteração de perfil em abas

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <a href="/home" class="btn btn-default">Voltar</a>
     @if ($errors->any())
         <div class="alert alert-danger">
@@ -15,83 +18,101 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <h2 class="card-header">Perfil de Usuário</h2>
-                    <br>
-                    <div class="card-body">
-                        <form action="{{ route('user.perfil', $user)}}" method="POST">
-                            @csrf
-                            <div class="form-group row">
-                                <label for="nome" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
-                                <div class="col-md-6">
-                                    <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome"
-                                        value="{{ $user->name }}">
-                                    @error('nome')
-                                        <div class="invalid-feedback" style="color: red">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
+                    <h2 class="card-header">Perfil do Usuário</h2>
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-toggle="tab" href="#base" role="tab">Informações Básicas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#senha" role="tab">Alterar Senha</a>
+                        </li>
+                    </ul>
+                    <div class="card-body tab-content">
+                        <div class="tab-pane active" id="base" role="tabpanel">
+                            <h3>Alterar Informações Básicas</h3>
+                            <br>
+                            <form action="{{ route('user.perfil', $user) }}" method="POST">
+                                @csrf
+                                <div class="form-group row">
+                                    <label for="nome"
+                                        class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
+                                    <div class="col-md-6">
+                                        <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome"
+                                            value="{{ $user->name }}">
+                                        @error('nome')
+                                            <div class="invalid-feedback" style="color: red">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Endereço de Email') }}</label>
-                                <div class="col-md-6">
-                                    <input type="text" class="form-control" id="email" name="email" placeholder="Email"
-                                        value="{{ $user->email }}">
-                                    @error('email')
-                                        <div class="invalid-feedback" style="color: red">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="email"
+                                        class="col-md-4 col-form-label text-md-right">{{ __('Endereço de Email') }}</label>
+                                    <div class="col-md-6">
+                                        <input type="text" class="form-control" id="email" name="email"
+                                            placeholder="Email" value="{{ $user->email }}">
+                                        @error('email')
+                                            <div class="invalid-feedback" style="color: red">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row col-md-4">
-                                <input type="submit" class="btn btn-success" value="Alterar Perfil">
-                            </div>
-                        </form>
-                        <br>
-                        <br>
-                        <br>
+                                <div class="form-group row col-md-4">
+                                    <input type="submit" class="btn btn-success" value="Alterar Perfil">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="tab-pane" id="senha" role="tabpanel">
                             <h3>Alterar Senha</h3>
                             <br>
-                        <form action="{{ route('user.update', $user)}}" method="POST">
+                            <form action="{{ route('user.update', $user) }}" method="POST">
                                 @csrf
-                            <div class="form-group row">
-                                <label for="current_password" class="col-md-4 col-form-label text-md-right">{{ __('Senha Atual') }}</label>
-                                <div class="col-md-6">
-                                    <input type="password" class="form-control" id="current_password" name="current_password" placeholder="Senha Atual">
-                                    @error('current_password')
-                                        <div class="invalid-feedback" style="color: red">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="current_password"
+                                        class="col-md-4 col-form-label text-md-right">{{ __('Senha Atual') }}</label>
+                                    <div class="col-md-6">
+                                        <input type="password" class="form-control" id="current_password"
+                                            name="current_password" placeholder="Senha Atual">
+                                        @error('current_password')
+                                            <div class="invalid-feedback" style="color: red">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Nova Senha') }}</label>
-                                <div class="col-md-6">
-                                    <input type="password" class="form-control" id="password" name="password" placeholder="Nova Senha">
-                                    @error('password')
-                                        <div class="invalid-feedback" style="color: red">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="password"
+                                        class="col-md-4 col-form-label text-md-right">{{ __('Nova Senha') }}</label>
+                                    <div class="col-md-6">
+                                        <input type="password" class="form-control" id="password" name="password"
+                                            placeholder="Nova Senha">
+                                        @error('password')
+                                            <div class="invalid-feedback" style="color: red">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Senha') }}</label>
-                                <div class="col-md-6">
-                                    <input type="password" class="form-control" id="password-confirm" name="password-confirm" placeholder="Confirmar Senha">
-                                    @error('password-confirm')
-                                        <div class="invalid-feedback" style="color: red">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="password-confirm"
+                                        class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Senha') }}</label>
+                                    <div class="col-md-6">
+                                        <input type="password" class="form-control" id="password-confirm"
+                                            name="password-confirm" placeholder="Confirmar Senha">
+                                        @error('password-confirm')
+                                            <div class="invalid-feedback" style="color: red">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row col-md-4">
-                                <input type="submit" class="btn btn-success" value="Alterar Senha">
-                            </div>                            
-                        </form>
+                                <div class="form-group row col-md-4">
+                                    <input type="submit" class="btn btn-success" value="Alterar Senha">
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
