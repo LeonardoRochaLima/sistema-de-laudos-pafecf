ee1bca5d2521bfa583107644b48377a1e607cd2c
Leonardo Lima <leonardo.lima@edu.unifil.br>
2021-09-29 11:57:00 -0300

    Atualizando Formulário de cadastro de empresas - Abas e botões next - prev

--- resources/views/cadastros/create.blade.php
+++ resources/views/cadastros/create.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/cadastros" class="btn btn-default">Empresas Cadastradas</a>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <h1>Cadastrar Empresa</h1>
-
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
@@ -14,16 +14,42 @@
             </ul>
         </div>
     @endif
-
     <div id="cadastrar-empresa-create-container">
         <form action="/cadastros" method="POST" name="formulario">
             @csrf
-            <div class="form-group col-md-7">
+            <div class="form-group col-md-12">
                 <h3>Empresa Desenvolvedora Requerente: </h3>
                 <small>Os campos obrigatórios estão representados com um asterisco (*).</small>
             </div>
-            <div class="title-body">
-                <form class="row">
+            <ul class="nav nav-tabs" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" data-toggle="tab" href="#base" role="tab">Informações Básicas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-toggle="tab" href="#enderecos" role="tab">Endereço</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-toggle="tab" href="#representante" role="tab">Informações do
+                        Representante</a>
+                </li>
+            </ul>
+            <script type="text/javascript">
+                $(document).ready(function() {
+                    $('.proximo').click(function() {
+                        $('.nav-tabs .active').parent().next('li').find('a').trigger('click');
+                    });
+
+                    $('.anterior').click(function() {
+                        $('.nav-tabs .active').parent().prev('li').find('a').trigger('click');
+                    });
+                });
+            </script>
+            <br>
+            <div class="title-body tab-content">
+                <div class="tab-pane active" id="base" role="tabpanel">
+                    <div class="form-group col-md-12" id="botoes">
+                        <a class="btn btn-primary proximo pull-right">Próximo</a>
+                    </div>
                     <div class="form-group control-label col-md-7">
                         <label for="razao_social">Razão Social</label>
                         <input type="text" class="form-control @error('razao_social') is-invalid @enderror"
@@ -54,6 +80,49 @@
                         @enderror
                     </div>
                     <div class="form-group control-label col-md-4">
+                        <label for="telefone">Telefone</label>
+                        <input type="text" class="form-control" id="telefone" name="telefone" placeholder="Telefone">
+                        @error('telefone')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group control-label col-md-4">
+                        <label for="celular">Celular</label>
+                        <input type="text" class="form-control" id="celular" name="celular" placeholder="Celular">
+                        @error('celular')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="inscricao_estadual">Inscrição Estadual</label>
+                        <input type="text" class="form-control" id="inscricao_estadual" name="inscricao_estadual"
+                            placeholder="Inscrição Estadual">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="inscricao_municipal">Inscrição Municipal</label>
+                        <input type="text" class="form-control" id="inscricao_municipal" name="inscricao_municipal"
+                            placeholder="Inscriação Municipal">
+                    </div>
+                </div>
+                <div class="tab-pane" id="enderecos" role="tabpanel">
+                    <div class="form-group col-md-12" id="botoes">
+                        <a class="btn btn-primary proximo pull-right">Próximo</a>
+                        <a class="btn btn-primary anterior">Voltar</a>
+                    </div>
+                    <div class="form-group control-label col-md-4">
+                        <label for="cidade">Cidade</label>
+                        <input type="text" class="form-control" id="cidade" name="cidade" placeholder="Cidade">
+                        @error('cidade')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group control-label col-md-4">
                         <label for="endereco">Endereço</label>
                         <input type="text" class="form-control" id="endereco" name="endereco" placeholder="Endereço">
                         @error('endereco')
@@ -66,15 +135,6 @@
                         <label for="bairro">Bairro</label>
                         <input type="text" class="form-control" id="bairro" name="bairro" placeholder="Bairro">
                         @error('bairro')
-                            <div class="invalid-feedback" style="color: red">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-                    <div class="form-group control-label col-md-6">
-                        <label for="cidade">Cidade</label>
-                        <input type="text" class="form-control" id="cidade" name="cidade" placeholder="Cidade">
-                        @error('cidade')
                             <div class="invalid-feedback" style="color: red">
                                 {{ $message }}
                             </div>
@@ -98,35 +158,12 @@
                             </div>
                         @enderror
                     </div>
-                    <div class="form-group control-label col-md-6">
-                        <label for="telefone">Telefone</label>
-                        <input type="text" class="form-control" id="telefone" name="telefone" placeholder="Telefone">
-                        @error('telefone')
-                            <div class="invalid-feedback" style="color: red">
-                                {{ $message }}
-                            </div>
-                        @enderror
+                </div>
+                <div class="tab-pane" id="representante" role="tabpanel">
+                    <div class="form-group col-md-12" id="botoes">
+                        <a class="btn btn-primary anterior">Voltar</a>
                     </div>
                     <div class="form-group control-label col-md-6">
-                        <label for="celular">Celular</label>
-                        <input type="text" class="form-control" id="celular" name="celular" placeholder="Celular">
-                        @error('celular')
-                            <div class="invalid-feedback" style="color: red">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="inscricao_estadual">Inscrição Estadual</label>
-                        <input type="text" class="form-control" id="inscricao_estadual" name="inscricao_estadual"
-                            placeholder="Inscrição Estadual">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="inscricao_municipal">Inscrição Municipal</label>
-                        <input type="text" class="form-control" id="inscricao_municipal" name="inscricao_municipal"
-                            placeholder="Inscriação Municipal">
-                    </div>
-                    <div class="form-group control-label col-md-4">
                         <label for="representante">Representante Legal da Empresa</label>
                         <input type="text" class="form-control" id="representante" name="representante"
                             placeholder="Representante Legal da Empresa">
@@ -136,17 +173,7 @@
                             </div>
                         @enderror
                     </div>
-                    <div class="form-group control-label col-md-4">
-                        <label for="cpf_representante">CPF do Responsável</label>
-                        <input type="text" class="form-control" id="cpf_representante" name="cpf_representante"
-                            placeholder="CPF do Representante">
-                        @error('cpf_representante')
-                            <div class="invalid-feedback" style="color: red">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-                    <div class="form-group control-label col-md-4">
+                    <div class="form-group control-label col-md-6">
                         <label for="rg_representante">RG do Representante</label>
                         <input type="text" class="form-control" id="rg_representante" name="rg_representante"
                             placeholder="RG do Representante">
@@ -156,7 +183,17 @@
                             </div>
                         @enderror
                     </div>
-                    <div class="form-group control-label col-md-12">
+                    <div class="form-group control-label col-md-6">
+                        <label for="cpf_representante">CPF do Representante</label>
+                        <input type="text" class="form-control" id="cpf_representante" name="cpf_representante"
+                            placeholder="CPF do Representante">
+                        @error('cpf_representante')
+                            <div class="invalid-feedback" style="color: red">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group control-label col-md-6">
                         <label for="email_representante">Email de Contato - Representante</label>
                         <input type="text" class="form-control" id="email_representante" name="email_representante"
                             placeholder="Email">
@@ -169,8 +206,8 @@
                     <div class="form-group col-md-2">
                         <input type="submit" class="btn btn-success" value="Finalizar Cadastro">
                     </div>
-                    <br>
-                </form>
+                </div>
+                <br>
             </div>
         </form>
     </div>
